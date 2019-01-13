f8911961d2ad2df44a527f718a2bbe958ff6be1b
Mango <mangoxiaoda@gmail.com>
2019-01-13 08:23:46 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,14 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
 
-
+    /* 创建 */
     public function create()
     {
         return view('users.create');
+    }
+
+    // 显示用户个人信息页面
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    // 表单验证
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'    => 'required|max:50',
+            'email'   => 'required|email|unique:users|max:255',
+            'password'=> 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name'    => $request->name,
+            'email'   => $request->email,
+            'password'=> bcrypt($request->password),
+
+        ]);
+
+        session()->flash('success','注册成功，祝您有一段新的旅程！');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 // 注册
 Route::get('signup', 'UsersController@create')->name('signup');
+
+// 用户资源路由
+Route::resource('users', 'UsersController');
+
